a2e55c1f760f33c63b2948d12c5e3276a213dcec
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-06-19 01:16:56 +0530

    functions

---
+++ functions.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void AFunction() {
+    printf("Hello From A Function\n");
+}
+
+void AnotherFunction() {
+    printf("Hello From Another Function\n");
+}
+
+int main() {
+    printf("Hello from main function\n");
+    AFunction();
+    AnotherFunction();
+}
